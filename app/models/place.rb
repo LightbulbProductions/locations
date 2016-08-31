298c6e29d4250431c9b90529b5e013ad41fdf8b5
LightbulbProductions <gerbermichelle23@gmail.com>
2016-08-31 16:06:30 +0000

    adding base files/ issue with map

---
+++ app/models/place.rb
@@ -0,0 +1,14 @@
+class Place < ActiveRecord::Base
+  validates_presence_of :name
+  validates_presence_of :phone
+  validates_presence_of :address
+  validates_presence_of :website
+  validates_presence_of :user_id
+  
+  belongs_to :user
+  
+  geocoded_by :address   # can also be an IP address
+  after_validation :geocode          # auto-fetch coordinates
+  
+  has_many :reviews, dependent: :destroy
+end
